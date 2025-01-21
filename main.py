aea7734651db4306836ed3ef916981d0b94a1b0c
Harshit Dawar <31154755+HarshitDawar55@users.noreply.github.com>
2025-01-21 23:47:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ app = Flask("OpenShift S2I 101")
 @app.route("/", methods = ["GET"])
 def main():
   return "This is OpenShift Source 2 Image demo by Mr. Harshit Dawar!"
+
+app.run(host="0.0.0.0", port=80)
